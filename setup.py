f8b040a97bb3bb129ad0021f5345810c29abf4bb
Ahmad Alhour <a.z.alhour@gmail.com>
2016-05-25 23:40:14 +0200

    Added setup.py and updated .gitignore

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='PyCOOLC',
+    version='0.0.1',
+    description='A compiler for the COOL Programming Language written entirely in Python 3.',
+    author='Ahmad Alhour',
+    author_email='a.z.alhour@gmail.com',
+    url='https://github.com/aalhour/PyCOOLC',
+    packages=['PyCOOLC'],
+    install_requires=['ply']
+)
+
