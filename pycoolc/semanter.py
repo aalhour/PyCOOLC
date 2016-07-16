eddac9a52fed9c377a9b0c846a84cf6a7611ddfa
Ahmad Alhour <a.z.alhour@gmail.com>
2016-07-16 13:03:23 +0200

    Semantic Analysis: Added semanter.py module (WIP).

---
+++ pycoolc/semanter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# -----------------------------------------------------------------------------
+# semanter.py
+#
+# Author:       Ahmad Alhour (aalhour.com).
+# Date:         TODO
+# Description:  The Semantic Analyser module. Implements Semantic Analysis and
+#               Type Checking.
+# -----------------------------------------------------------------------------
+
+import sys
+import ast as AST
+from parser import make_parser
+
+
+# -----------------------------------------------------------------------------
+#                Semantic Analyser as a Standalone Python Program
+#                Usage: ./semanter.py cool_program.cl
+# -----------------------------------------------------------------------------
+
+
+def make_semanter(**kwargs):
+    """
+    Utility function.
+    :return: PyCoolSemanter object.
+    """
+    raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    print("Not implemented!")
+    exit(1)
+
